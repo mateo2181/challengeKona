7c609c80b8a7000a8a8d0a6042ce49b4a6ecc8a7
mateo2181 <mateomerlo@gmail.com>
2021-12-15 16:29:33 -0300

    Fix: TeamTree component.

--- client/src/components/TeamTree/index.tsx
+++ client/src/components/TeamTree/index.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { useQueryClient } from 'react-query';
+import useTeams from '../../hooks/useTeams';
 import { Team as ITeam } from '../../types';
 import Team from '../Team';
 import styles from './TeamTree.module.scss';
 
 export default function TeamTree () {
   const [primaryTeams, setPrimaryTeams] = useState<ITeam[]>([]);
-  const queryClient = useQueryClient();
-  const teams = queryClient.getQueryData<ITeam[]>('teams');
+  // const queryClient = useQueryClient();
+  // const teams = queryClient.getQueryData<ITeam[]>('teams');
+  const { isLoading, data: teams, error } = useTeams();
 
   useEffect(() => {
     console.log(teams);
@@ -15,6 +16,10 @@ export default function TeamTree () {
       setPrimaryTeams(teams.filter((team: ITeam) => team.settings.channel_id));
     }
   }, [teams]);
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (error) return <div>An error has occurred: {error.message}</div>;
 
   return (
           <ul className={styles.list}>
--- client/src/pages/TeamsPage/index.tsx
+++ client/src/pages/TeamsPage/index.tsx
@@ -5,15 +5,16 @@ import useTeams from '../../hooks/useTeams';
 import styles from './TeamPage.module.scss';
 
 export default function TeamsPage () {
-  const { isLoading, error } = useTeams();
+  // const { isLoading, error } = useTeams();
 
-  if (error) return <div>An error has occurred: {error.message}</div>;
+  // if (error) return <div>An error has occurred: {error.message}</div>;
 
   return (
     <div className={styles.container}>
           <div className={styles.sidebar}>
               <h1>Teams</h1>
-              { isLoading ? <div>Loading...</div> : <TeamTree /> }
+              <TeamTree />
+              {/* { isLoading ? <div>Loading...</div> : <TeamTree /> } */}
           </div>
           <div className={styles.detail}>
             <Outlet />
